91debabae669aeec1c0f5cb22650504ffd772a4f
Austin Bell <xavierbellhaddon@gmail.com>
2020-10-11 18:39:58 -0400

    first commit

---
+++ src/api/index.js
@@ -0,0 +1,46 @@
+import axios from "axios";
+
+const url = "https://covid19.mathdro.id/api";
+
+export const fetchData = async (country) => {
+    let changeableURL = url;
+    if (country) {
+        changeableURL = `${url}/countries/${country}`
+    }
+  try {
+    const {
+      data: { confirmed, recovered, deaths, lastUpdate },
+    } = await axios.get(changeableURL);
+
+    return { confirmed, recovered, deaths, lastUpdate };
+  } catch (error) {
+    return error;
+  }
+};
+
+export const fetchDailyData = async () => {
+  try {
+    const { data } = await axios.get(
+      "https://api.covidtracking.com/v1/us/daily.json"
+    );
+
+    const modifiedData = data.map((dailyData) => ({
+        confirmed: dailyData.positive, 
+        deaths: dailyData.death,
+        date: dailyData.dateChecked
+    }))
+    return modifiedData;
+  } catch (error) {
+    return error;
+  }
+};
+
+export const fetchCountries = async () => {
+    try {
+        const { data: { countries }} = await axios.get(`${url}/countries`);
+        // console.log('countries: ', response);
+        return countries.map((country) => country.name)
+    } catch (error) {
+        console.log(error)
+    }
+}
